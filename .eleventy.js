bdd8eef094de5ac4fff44ae601a1db7505cff6f1
Karl Haworth <git@karlhaworth.com>
2020-08-07 09:48:54 -0700

    updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,7 @@
-const sitemap = require("@quasibit/eleventy-plugin-sitemap");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(sitemap, {
-    sitemap: {
-      hostname: "https://karlhaworth.com",
-    },
-  });
   eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy({ "node_modules/bootstrap/dist/**/*.js": "vendor", "node_modules/jquery/dist/**/*.js": "vendor", "node_modules/popper.js/dist/**/*.js": "vendor"  });
+  eleventyConfig.addPassthroughCopy( "node_modules/bootstrap/dist/" );
+  eleventyConfig.addPassthroughCopy( "node_modules/jquery/dist/" );
+  eleventyConfig.addPassthroughCopy( "node_modules/popper.js/dist/" );
 };
