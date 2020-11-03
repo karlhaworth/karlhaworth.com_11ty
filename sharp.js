64331666a20e2b6339b7cd1264fae781203f5bd5
Karl Haworth <git@karlhaworth.com>
2020-11-03 07:31:07 -0700

    update

--- sharp.js
+++ sharp.js
@@ -1,20 +1,20 @@
 const sharp = require('sharp');
 const fs = require('fs');
 
-const testFolder = './assets/technologies/';
+const folder = 'assets/technologies/';
 
-fs.readdirSync(testFolder).forEach(file => {
+fs.readdirSync(folder).forEach(file => {
   if(file.includes('.png')) {
         filename = file.slice(0, -4)
         console.log(file)
-        sharp(testFolder + file)
+        sharp(folder + file)
             .resize({ height: 40 })
-            .toFile(testFolder + 'retina/' + filename + '.png');
-        sharp(testFolder + file)
+            .toFile(folder + 'retina/' + filename + '.png');
+        sharp(folder + file)
             .resize({ height: 80 })
-            .toFile(testFolder + 'retina/' + filename + '@2x.png');
-        sharp(testFolder + file)
+            .toFile(folder + 'retina/' + filename + '@2x.png');
+        sharp(folder + file)
             .resize({ height: 120 })
-            .toFile(testFolder + 'retina/' + filename + '@3x.png');
+            .toFile(folder + 'retina/' + filename + '@3x.png');
   }
 })
